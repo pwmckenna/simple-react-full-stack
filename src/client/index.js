811cbdbe5753f3f37f332497ac4d5b37586fc1c4
Patrick Williams <pwmckenna@gmail.com>
2018-12-06 07:45:05 -0800

    init

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import Fluxible from 'fluxible';
+import FluxibleContext from 'fluxible-context';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import fetchrPlugin from 'fluxible-plugin-fetchr';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const fluxible = new Fluxible();
+
+fluxible.plug(fetchrPlugin({
+  xhrPath: '/api' // Path for XHR to be served from
+}));
+
+ReactDOM.render((
+  <FluxibleContext.Provider value={fluxible.createContext().getActionContext()}>
+    <Suspense fallback={<div>loading...</div>}>
+      <App />
+    </Suspense>
+  </FluxibleContext.Provider>
+), document.getElementById('root'));
